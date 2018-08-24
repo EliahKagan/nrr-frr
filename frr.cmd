f22370fabd264464889df75fd5aeab3af1be546f
Eliah Kagan <degeneracypressure@gmail.com>
2018-08-24 01:48:28 -0400

    just use frr literally, for now

--- frr.cmd
+++ frr.cmd
@@ -1,5 +1,7 @@
 @echo off
 rem frr.cmd - utility to look up an existing remote repo
 
-rem TODO: Is "%0" really being escaped adequatly? Might it contain quote marks?
-%GIT_SSH% team-roach.ddns.net "env FINDREPO_REMOTE_FRONTEND_COMMAND_NAME=%0 findrepo %*"
+rem TODO: Is there some way to use %0 instead of frr, where backslashes (andd
+rem       other specially treated characters) are quoted for both the client
+rem       shell (that runs this script) and the server shell (that SSH runs)?
+%GIT_SSH% team-roach.ddns.net "env FINDREPO_REMOTE_FRONTEND_COMMAND_NAME=frr findrepo %*"
