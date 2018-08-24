3e1c6d23afcce66a967ce41f02d1daaf296f419e
Eliah Kagan <degeneracypressure@gmail.com>
2018-08-24 01:56:52 -0400

    I could have sworn I had typed that equals sign.

--- frrd.cmd
+++ frrd.cmd
@@ -5,4 +5,4 @@ rem utility to look up an existing remote repo
 rem TODO: Is there some way to use %0 instead of frrd, where backslashes (and
 rem       other specially treated characters) are quoted for both the client
 rem       shell (that runs this script) and the server shell (that SSH runs)?
-%GIT_SSH% team-roach.ddns.net "env FINDREPO_REMOTE_FRONTEND_COMMAND_NAME=frrd DEBUG_COSINE_SIMILARITY findrepo %*"
+%GIT_SSH% team-roach.ddns.net "env FINDREPO_REMOTE_FRONTEND_COMMAND_NAME=frrd DEBUG_COSINE_SIMILARITY= findrepo %*"
